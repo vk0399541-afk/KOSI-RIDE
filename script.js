ea8a1a7d642a61b0b3074ee1eddb5e8f39a64352
vk0399541-afk <vk0399541@gmail.com>
2026-07-22 20:35:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -420,7 +420,8 @@ async function showRoute(pickup, destination) {
         .bindPopup("Destination");
 
     const res = await fetch(
-`https://api.heigit.org/openrouteservice/v2/directions/driving-car?api_key=${ORS_API_KEY}&start=${pickupCoord[0]},${pickupCoord[1]}&end=${destinationCoord[0]},${destinationCoord[1]}`
+https://api.openrouteservice.org/v2/directions/driving-car
+=${ORS_API_KEY}&start=${pickupCoord[0]},${pickupCoord[1]}&end=${destinationCoord[0]},${destinationCoord[1]}`
 );
 
     const data = await res.json();
