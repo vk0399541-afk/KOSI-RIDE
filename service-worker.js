ba1e9ddb8c98a32ff2ba27e5264ab23f51a02cd6
vk0399541-afk <vk0399541@gmail.com>
2026-07-23 10:25:51 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
-b
+const CACHE_NAME = "kosi-ride-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
